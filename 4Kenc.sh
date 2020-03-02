fc9cd58f05ac441839f596ec07c3b3f3033ef060
Pekka Nykänen <pekka.nykanen@outlook.com>
2020-03-02 20:52:23 +0200

    filename edit

--- 4Kenc.sh
+++ 4Kenc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for name in "$@"; do
-ffmpeg -y -i "$name" -c:v libx265 -b:v 8192k -x265-params pass=1 -an -f mp4 /dev/null && \
-ffmpeg -i "$name" -c:v libx265 -b:v 8192k -x265-params pass=2 -an "$name.reEncoded.mkv"
-touch "$name.reEncoded.mkv" -r "$name"
+nice -n 19 ffmpeg -y -i "$name" -c:v libx265 -b:v 8192k -x265-params pass=1 -an -f mp4 /dev/null && \
+nice -n 19 ffmpeg -i "$name" -c:v libx265 -b:v 8192k -x265-params pass=2 -an "$name.x265.mkv"
+touch "$name.x265.mkv" -r "$name"
 rm x265_2pass.log.cutree
 rm x265_2pass.log
 done
